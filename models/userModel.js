584e7bd927a3397dff296a47f47ecbd930114cc7
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2022-06-08 18:07:11 +0300

    fix: change userModel file name & update min-max-Length property

---
+++ models/userModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: [2, 'Минимальная длина - 2 символа'],
+    maxlength: [30, 'Максимальная длина - 30 символов'],
+  },
+  about: {
+    type: String,
+    required: true,
+    minlength: [2, 'Минимальная длина - 2 символа'],
+    maxlength: [30, 'Максимальная длина - 30 символов'],
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+});
+
+exports.User = mongoose.model('user', userSchema);
+
