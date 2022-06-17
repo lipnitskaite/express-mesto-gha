cc3225d03f91e7db18c64cbe3ae73f166f71b714
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2022-06-17 14:41:02 +0300

    fix: models

--- models/cardModel.js
+++ models/cardModel.js
@@ -5,8 +5,8 @@ const cardSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    minlength: 2,
-    maxlength: 30,
+    minlength: [2, 'Минимальная длина - 2 символа'],
+    maxlength: [30, 'Максимальная длина - 30 символов'],
   },
   link: {
     type: String,
@@ -19,6 +19,8 @@ const cardSchema = new mongoose.Schema({
   },
   likes: [{
     type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    default: []
   }],
   createdAt: {
     type: Date,
